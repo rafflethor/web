6e611553ce54c5254b48ef8cd73436f9b88d4765
Xaviju <xaviju@gmail.com>
2018-07-18 11:10:55 +0200

    Back to begginign

--- src/pages/registry/Registry.js
+++ src/pages/registry/Registry.js
@@ -5,13 +5,15 @@ import './Registry.css';
 class Registry extends Component {
   render() {
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload x.
-        </p>
-      </header>
+      <main className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload x.
+          </p>
+        </header>
+      </main>
     );
   }
 }
