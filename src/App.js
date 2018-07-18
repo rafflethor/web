db66127be7673723bf69f40378a247c9c09fb46a
Xaviju <xaviju@gmail.com>
2018-07-18 10:19:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Registry from './pages/registry/Registry';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Registry />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/registry/Registry.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './Registry.css';
+
+class Registry extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Registry;
